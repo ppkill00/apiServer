2dca9937f33e7fbc9d94c7e688f8eaff44cb2c51
ParkJiHo <ppkill00@gmail.com>
2019-08-29 00:53:22 +0900

    test2

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 # Process https://docs.python.org/2/library/multiprocessing.html
 # schedule jobs parallel https://schedule.readthedocs.io/en/stable/faq.html#how-to-execute-jobs-in-parallel
 # multiprocess https://niceman.tistory.com/145?category=940952
+# debug 를 사용하면 스레드가 2번 실행이 된다. 리로더의 문제인걸로 보인다. 
 
 import schedule, signal, queue, threading,time, os, datetime,uuid
-from multiprocessing import Process
+# from multiprocessing import Process
+import multiprocessing
 
 from flask import Flask, request
 import logging
@@ -23,8 +25,6 @@ logger = logging.getLogger('automatorLogger')
 
 app = Flask(__name__)
 
-t = None
-job_timer = None
 
 
 def sigint_handler(signum, frame):
@@ -38,41 +38,19 @@ def sigint_handler(signum, frame):
 
 def job():
     logger.warn('i am here to die%s'%threading.current_thread())
-    print('test')
-
-#####################
-
-
-
-def worker_main():
-    while 1:
-        job_func = jobqueue.get()
-        job_func()
-        jobqueue.task_done()
-
-jobqueue = queue.Queue()
-
-
-worker_thread = threading.Thread(target=worker_main)
-worker_thread.start()
-
-def run_schedule():
-    """ infinite loop for schedule """
-    print('start schedule')
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-
 
 schedule.every(2).seconds.do(job)
 
 
-if os.environ['ENV'] == "DEV":
-    schedule.every(2).seconds.do(jobqueue.put, job)
-    #schedule.every(5).seconds.do(batchJob().startAllBatch) #30min
-    #schedule.every(10).seconds.do(developerReport.createCont)
+#####################
+
+# @app.before_first_request
+# def activate_job():
+def run_job():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 
 
@@ -83,7 +61,6 @@ def index():
 
 @app.route('/whoami/<string:name>')
 def whoami(name):
-
     return 'whoami page<br>' + name
 
 
@@ -93,17 +70,11 @@ def mytimer():
     return "end program"
  
 
-
 signal.signal(signal.SIGINT, sigint_handler)  #ctrl-c 예외 처리.
 
 
 if __name__ == '__main__':
-    p = Process(target=app.run(host='localhost', port=5000,debug=True))
-    t = Process(target=run_schedule)
-    t.start()
-
-    p.start()
-    t.join()
-
-    p.join()
+    thread = threading.Thread(target=run_job)
+    thread.start()
+    app.run(host='localhost', port=5000,debug=False)
 
